d1de0fe5b8832df57d32b9da9bb5cef456fc9786
albertgwo <albertgwo@gmail.com>
2026-04-20 15:47:56 -0400

    chore(linear): prepare package for publish

--- linear/cli/lib/config.js
+++ linear/cli/lib/config.js
@@ -5,7 +5,7 @@ import { join } from "node:path";
 export const TOOL_NAME = "linear";
 export const PACKAGE_NAME = "@ruminaider/linear-cli";
 export const CLI_NAME = "linear-cli";
-export const PACKAGE_VERSION = "0.0.0";
+export const PACKAGE_VERSION = "0.1.0";
 
 export const RESERVED_COMMAND_NAMESPACES = Object.freeze([
   "auth",
